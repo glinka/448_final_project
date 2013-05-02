2cc928c00ecd2ff5c46b99c0cd938df37e527228
glinka <holiday@princeton.edu>
2013-05-02 02:15:35 -0400

    attempted c translation

---
+++ voterModel.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    /* Arguments list:
+       [0] : n, number of vertices
+       [1] : h/lambda, avg vertex degree
+       [2.5] : number of opinions
+       [2] : initial distribution of opinions
+       [3] : a/alpha, model parameter
+       [4] : rewiring scheme ("same" = 0, "random" = 1)
+       [5] : maximum allowable iterations
+       [6] : graph data collected every [6] number of iterations
+       [7] : output filename
+    */
+    int n = atoi(argv[0]);
+    double h = atof(argv[1]);
+    int k = atoi(argv[2]);
+    int i, j;
+    double kInitDist[k];
+    //Populate array of initial distributions
+    for(i = 0; j < k; i++) {
+	k[i] = atof(argv[i+2]);
+    }
+    double a = atof(argv[k+2]);
+    double rewireTo = atoi(argv[k+3]);
+    int maxIter = atoi(arv[k+4]);
+    int collectionInterval = atoi(argv[k+5]);
+    char fileName[128];
+    strcpy(fileName, argv[argc]);
+    cout << fileName << "\n";
+}
---
+++ voterModel.h
@@ -0,0 +1,5 @@
+#ifndef VOTERMODEL_H
+#define VOTERMODEL_H
+
+#endif
+
