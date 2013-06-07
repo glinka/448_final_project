fa838e6979e5aa0818a687b5a28b33f57e1c1f89
tchaikovsky <holiday@princeton.edu>
2013-06-07 19:13:04 -0400

    vastly improved graphing of bifurcation data

--- plotOutput.py
+++ plotOutput.py
@@ -41,22 +41,52 @@ def plotGraphStats(fileName):
         ax.set_ylim([l2Low, l2Up])
         plt.show()
     elif "bifData" in fileName:
-        toPlot = filter(lambda x: x[2] == 0, toPlot)
+        toPlot = filter(lambda x: ((x[2] == 0) & (x[1] != 0)), toPlot)
         toPlot = np.asarray(toPlot)
         n = toPlot.shape[0]
-        #f1 = plt.figure(num=1)
-        #ax = f1.add_subplot(111)
+        #ids is a 2d list, with the first holding
+        #the different values of alpha, the second
+        #the corresponding initial minority fraction
+        ids = {}
         colorList = []
+        nu0 = 0
+        nAlpha = 0
         for i in range(n):
-            color = toPlot[i,3]
-            if not color in colorList:
-                plt.plot(toPlot[i,0], toPlot[i,1],'o',color=[1-2*color, np.cos(np.pi*color), 2*color], label="initial fraction= " + str(color))
-                colorList.append(color)
-            else:
-                plt.plot(toPlot[i,0], toPlot[i,1],'o',color=[1-2*color, np.cos(np.pi*color), 2*color])
+            alpha = int(100*toPlot[i,0])
+            u0 = int(100*toPlot[i,3])
+            uf = toPlot[i,1]
+            if not alpha in ids:
+                ids[alpha] = {}
+            if not u0 in ids[alpha]:
+                ids[alpha][u0] = []
+            ids[alpha][u0].append(uf)
+            nAlpha = len(ids)
+            if len(ids[alpha]) > nu0:
+                nu0 = len(ids[alpha])
+        toPlot = np.zeros((nu0,nAlpha,3))
+        u0s = []
+        j = 0
+        for a in ids:
+            u0s = ids[a].keys()
+            u0s.sort()
+            i = 0
+            for u0 in u0s:
+                ids[a][u0] = np.average(np.asarray(ids[a][u0]))
+                toPlot[i,j,0] = a/100.0
+                toPlot[i,j,1] = ids[a][u0]
+                i = i + 1
+            j = j + 1
+        print u0s
+        print len(u0s)
+        for i in range(nu0):
+            u0 = u0s[i]/100.0
+            plt.plot(toPlot[i,:,0], toPlot[i,:,1], 'o', color=[1-2*u0, np.cos(np.pi*u0), 2*u0], label="initial fraction= " + str(u0))
         plt.xlabel(r'$\alpha$', fontsize=20)
         plt.ylabel("Final minority fraction")
-        plt.legend()
+        colorList.sort()
+        plt.legend(loc=2)
+        plt.xlim([0,1])
+        plt.ylim([0,0.5])
         plt.show()
     
 if __name__=='__main__':
