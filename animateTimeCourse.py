28ae2d66b639d73b1618127c1e48fd65552c0a93
tchaikovsky <holiday@princeton.edu>
2013-07-15 16:41:23 -0400

    animation script improved

--- animateTimeCourse.py
+++ animateTimeCourse.py
@@ -9,17 +9,19 @@ import matplotlib.animation as animation
 
 def stepData(step, *data):
     i = 0
-    fader = 1
+    print step
     for d in data:
-        if(step > d.shape[0]-1):
-            fader = 2*step/d.shape[0]
-            step = d.shape[0]-1
-        ax.scatter(d[:step,1], d[:step,2], c=(d[:step,0]/fader), marker='*', cmap=fadeFromWhiteCmaps[i])
+        index = step
+        fader = 1.0
+        if(index > d.shape[0]-1):
+            fader = step/d.shape[0]
+            index = d.shape[0]-1
+        ax.scatter(d[:index,1], d[:index,2], c=(d[:index,0]/fader), s=8, marker='o', cmap=fadeFromWhiteCmaps[i], vmax=step, lw=0)
         i = i + 1
 
 def animateTimeCourse(inputFiles):
     global ax, fadeFromWhiteCmaps#, data
-    fadeFromWhiteCmaps = ['Blues', 'YlGn', 'BuGn', 'BuPu', 'YlOrRd', 'Greens', 'RdPu', 'OrRd', 'Greys', 'Purples']
+    fadeFromWhiteCmaps = ['Blues', 'YlOrRd', 'BuGn', 'BuPu', 'YlGn', 'Greens', 'RdPu', 'OrRd', 'Greys', 'Purples']
     data = []
     for f in inputFiles:
         pathToFile = os.path.realpath(f)
@@ -40,5 +42,8 @@ if __name__=="__main__":
     parser.add_argument('inputFiles', nargs='+')
     args = parser.parse_args()
     anim = animateTimeCourse(args.inputFiles)
-    writer = animation.FFMpegWriter(fps=30, bitrate=8000)
-    anim.save('graphStatEvo.mkv', writer=writer)
+    writer = animation.FFMpegWriter(fps=60, bitrate=8000)
+    fileName = args.inputFiles[0]
+    us = fileName.find("_")
+    fileName = "votingModelAnim" + fileName[us:-4] + ".mkv"
+    anim.save(fileName, writer=writer)
